2e6f52af5b4913c623cedf7793eed63ffa4b85b2
Sebastvin <Sebastvin@gmail.com>
2023-08-21 14:50:18 +0200

    Add counting words

--- wagtail_character_counter/static/character_counter.js
+++ wagtail_character_counter/static/character_counter.js
@@ -9,24 +9,37 @@ document.addEventListener('DOMContentLoaded', function() {
     let charCounter = document.createElement('div');
     charCounter.id = 'char-counter';
 
-    // Add the character counter directly below the editor container
+    // Create an element for the word counter
+    let wordCounter = document.createElement('div');
+    wordCounter.id = 'word-counter';
+
+    // Add the character and word counters directly below the editor container
     editorWrapper.parentNode.insertBefore(charCounter, editorWrapper.nextSibling);
+    editorWrapper.parentNode.insertBefore(wordCounter, charCounter.nextSibling);
 
     // Locate the contenteditable element
     let textField = editorWrapper.querySelector('.DraftEditor-editorContainer [contenteditable="true"]');
 
-    // Function to update the character count
+    // Function to update the character and word count
     const updateCounter = () => {
         // Get the raw text from the editor
         const textContent = textField.textContent || "";
+
+        // Count words. A word is defined as a sequence of non-space characters separated by spaces.
+        const wordCount = textContent.split(/\s+/).filter(Boolean).length;
+
         charCounter.innerText = `Characters: ${textContent.length}`;
+        wordCounter.innerText = `Words: ${wordCount}`;
     };
 
-    // Update the character count in response to changes in the editor
+    // Update the character and word count in response to changes in the editor
     textField.addEventListener('input', updateCounter);
 
     // Handle pasted text with a delay
     textField.addEventListener('paste', function(event) {
         setTimeout(updateCounter, 50);  // Update the counter after a short delay
     });
+
+    // Initial update to set initial values
+    updateCounter();
 });
